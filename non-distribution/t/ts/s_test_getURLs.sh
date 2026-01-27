59ecea475ec1407702b3d63661b1ab89b4d486f2
peter-popescu <peter_popescu@brown.edu>
2026-01-27 18:49:44 -0500

    fix: test missing exit def

--- non-distribution/t/ts/s_test_getURLs.sh
+++ non-distribution/t/ts/s_test_getURLs.sh
@@ -10,6 +10,8 @@ DIFF=${DIFF:-diff}
 
 url="https://cs.brown.edu/courses/csci1380/sandbox/1/level_1a/index.html"
 
+EXIT=0
+
 # no urls gives empty
 
 if ! $DIFF <(cat "$T_FOLDER"/d/sd0.txt | c/getURLs.js $url | sort) <(cat /dev/null) >&2;
